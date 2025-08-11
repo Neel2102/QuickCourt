533dc194a6d5c2d4dc5af53e6cdbd546aefb896f
Neel2102 <neelsutariya21@gmail.com>
2025-08-11 13:55:08 +0530

    userroutes changes and deletion of uneccesary files

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {userAuth,protectRoute} from '../middlewares/userAuth.js';
-import {getUserData, updateProfile}  from '../controllers/userController.js';
-
+import { userAuth, protectRoute } from '../middlewares/userAuth.js';
+import { getUserData, updateProfile } from '../controllers/userController.js';
+import uploadMiddleware from '../middlewares/uploadMiddleware.js'; // Import the middleware
 
 const userRouter = express.Router();
 
 userRouter.get('/data', userAuth, getUserData);
-userRouter.put('/update-profile',protectRoute,updateProfile);
+userRouter.put('/update-profile', protectRoute, uploadMiddleware.single('profilePic'), updateProfile);
 
 export default userRouter;
